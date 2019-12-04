503fa7c6a1aa332bcecade8ea32e2ebf4980e38b
Zack Amin <ishaqhamin@gmail.com>
2019-12-04 22:10:27 +0000

    Initial Commit

---
+++ Fate2Food/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fate2Food
+//
+//  Created by Ishaq on 04/12/2019.
+//  Copyright © 2019 Ishaq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
